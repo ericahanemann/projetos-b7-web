ec15d2ff8844945245184c4339978f9a3412f3e6
Érica Hanemann <ericavhanemann@gmail.com>
2022-10-14 10:33:32 -0300

    pizzas info

--- projeto-compra-de-pizzas/script.js
+++ projeto-compra-de-pizzas/script.js
@@ -1,18 +1,36 @@
+let modalQt = 1;
 const qs = (element) => document.querySelector(element);
 const qsa = (element) => document.querySelectorAll(element);
 
+//listagem das pizzas
 pizzaJson.map((item, index) => {
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault(); //cancela a ação padrão
 
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        qs('.pizzaBig img').src = pizzaJson[key].img;
+        qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        qs('.pizzaInfo--actualPrice').innerHTML = `R$${pizzaJson[key].price.toFixed(2)}`;
+        qs('.pizzaInfo--size.selected').classList.remove('selected');
+        qsa('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+        qs('.pizzaInfo--qt').innerHTML = modalQt;
+
         qs('.pizzaWindowArea').style.opacity = 0;
         qs('.pizzaWindowArea').style.display = 'flex';
-
         setTimeout(() => {
             qs('.pizzaWindowArea').style.opacity = 1;
         }, 50);
